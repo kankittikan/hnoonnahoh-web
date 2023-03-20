011c2277adff6791b606ed69f77ade3a7efd83fb
kankittikan <nongkan195@gmail.com>
2023-03-20 15:34:09 +0700

    change title

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>Eiei</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
